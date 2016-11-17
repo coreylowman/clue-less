7d477075c7eebb7200a15797244b0461a115df16
Jason <jason@simplerbio.com>
2016-11-17 17:45:52 -0500

    added client websocket and board

--- frontend/clueless.js
+++ frontend/clueless.js
@@ -1,1 +1,31 @@
-alert('hello world');
+function establishWebsocket() {
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+  //establish connection at localhost:3000
+    var connection = new WebSocket('ws://127.0.0.1:3000');
+
+    connection.onopen = function () {
+      console.log('opened');
+      document.getElementById('status').innerHTML = 'Connected';
+      // connection is opened and ready to use
+    };
+
+    connection.onerror = function (error) {
+        // an error occurred when sending/receiving data
+    };
+
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+            var json = JSON.parse(message.data);
+          console.log(json);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        // handle incoming message
+    };
+}
+
+establishWebsocket();

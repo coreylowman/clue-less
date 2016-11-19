26c914b686e46598eb1368faa29b75b2d687a969
Corey Lowman <clowman1993@gmail.com>
2016-11-19 16:09:04 -0500

    Adding method to get all ICard to suspect, room, weapon

--- server/src/main/java/edu/jhu/server/data/Weapon.java
+++ server/src/main/java/edu/jhu/server/data/Weapon.java
@@ -1,6 +1,8 @@
 package edu.jhu.server.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Weapon implements ICard, IBoardPiece {
@@ -8,6 +10,10 @@ public class Weapon implements ICard, IBoardPiece {
 
   public static Weapon get(String name) {
     return weapons.get(name);
+  }
+
+  public static List<Weapon> getAll() {
+    return new ArrayList<Weapon>(weapons.values());
   }
 
   private String name;

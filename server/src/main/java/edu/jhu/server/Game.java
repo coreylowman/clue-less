9b7a1abb8e58c74a99a51eaabc68f850ddcc5ee3
Corey Lowman <clowman1993@gmail.com>
2016-11-19 15:45:58 -0500

    Filling out Game & Player

--- server/src/main/java/edu/jhu/server/Game.java
+++ server/src/main/java/edu/jhu/server/Game.java
@@ -1,5 +1,55 @@
 package edu.jhu.server;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
 
+import org.json.JSONObject;
+
+import edu.jhu.server.data.CaseFile;
+
+public class Game {
+  private int currentTurnIndex;
+  private List<Player> players;
+  private CaseFile secretCards;
+  private Board board;
+  private boolean gameStarted;
+  private Timer timer;
+
+  public Game() {
+    this.currentTurnIndex = 0;
+    this.players = new ArrayList<Player>();
+    this.board = new Board();
+    this.gameStarted = false;
+    this.timer = new Timer();
+    // todo call timer.schedule() here for starting the game
+  }
+
+  public boolean isStarted() {
+    return this.gameStarted;
+  }
+
+  public boolean isFull() {
+    return this.players.size() == 6;
+  }
+
+  public void start() {
+    this.currentTurnIndex = 0;
+    this.gameStarted = true;
+    CardShuffler.shuffleAndDealCards(players);
+  }
+
+  public void addPlayer(Player player) {
+    players.add(player);
+  }
+
+  public void notifyPlayers(JSONObject event) {
+    for (Player player : players) {
+      player.sendEvent(event);
+    }
+  }
+
+  public void handleEvent(JSONObject event) {
+
+  }
 }
